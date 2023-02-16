ab005635a5901bff8a7163c7b8536455842172d7
Oskarnuz <oscar.nunez07@gmail.com>
2023-02-15 20:31:16 -0500

    Create post api product and creates  status for other requests

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
 const express = require('express');
-const bodyParser = require("body-parser");
+const morgan = require('morgan');
 const app = express();
-const {products, person} = require("./products")
+const {products, person} = require("./products");
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post('/api/products/:phoneNumber', (req, res) => {
-    let data = req.body.phoneNumber;
-    
-    res.send(data);
-    
-})
+app.use(morgan('tiny'));
+app.use(express.json());
+// Generar nueva peticion post 
+app.post('/api/products', (req, res) => {
+    const randomNumber = () => Math.floor(Math.random()* 1000) + 1
+    req.body.id = randomNumber();
+    if (!req.body.title || !req.body.price) return res.status(404).json({error: "missing title or price"});
+    const {title} = req.body
+    const repeatedTitle = products.find((product) => product.title === +title && product);
+    if (repeatedTitle) return res.status(400).json({error: "Most be unique"});
+    products.push(req.body);
+    res.status(200).json(req.body);
+});
 
+
+
+
+//Listar los productos
 app.get('/api/products', (req, res) => {
     res.status(200).json(products);
 });
-
+//Buscar por id un producto
 app.get('/api/products/:id', (req, res) => {
     const { id } = req.params
     const product = products.find((product) => product.id === +id && product);
-    if (!product) return res.status(404).send("The product doesn't exist");
+    if (!product) return res.status(404).json({error: "The product doesn't exist"});
     res.status(200).json(product);
 });
+
+// Eliminar una entrada
 app.delete('/api/products/:id', (req, res) => {
     const { id } = req.params
     const product = products.find((product) => product.id === +id && product);
-    res.send(`deleted product ${product.id}`);
+    res.status(200).send(`deleted product ${product.id}`);
 });
 
-
+//Informacion de productos
 app.get('/info', (req, res) => {
     const date = new Date();
-    res.send(`<p>Our store has info for ${products.length} products</p> \n <p>${date}</p>`);
+    res.status(200).send(`<p>Our store has info for ${products.length} products</p> \n <p>${date}</p>`);
 });
 
+//Listar las personas
 app.get('/about', (req, res) => {
-    res.json(person);
+    res.status(200).json(person);
 });
 app.listen(8000, () => console.log('Listening on port 8000'));
