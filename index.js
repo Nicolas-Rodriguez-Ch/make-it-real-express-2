9e5c0278b8588493793887521e28d4986a0012d6
Oskarnuz <oscar.nunez07@gmail.com>
2023-02-15 22:42:55 -0500

    Create post api product with error messages

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post('/api/products', (req, res) => {
     req.body.id = randomNumber();
     if (!req.body.title || !req.body.price) return res.status(404).json({error: "missing title or price"});
     const {title} = req.body
-    const repeatedTitle = products.find((product) => product.title === +title && product);
+    const repeatedTitle = products.find((product) => product.title === title && product);
     if (repeatedTitle) return res.status(400).json({error: "Most be unique"});
     products.push(req.body);
     res.status(200).json(req.body);
